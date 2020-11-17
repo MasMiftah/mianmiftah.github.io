3c3ea25cb937ba7f741a61541dc587d2dcb61db5
MasMiftah <74440247+MasMiftah@users.noreply.github.com>
2020-11-16 18:49:27 -0600

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,10 @@
-const myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hey';
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+  let mySrc = myImage.getAttribute('src');
+  if (mySrc == 'https://ih1.redbubble.net/image.465732423.4753/flat,1000x1000,075,f.jpg'){
+    myImage.setAttribute('src', 'https://www.pngkey.com/png/full/18-182565_eiffel-tower-500-x-500.png');
+  }else{
+    myImage.setAttribute('src', 'https://ih1.redbubble.net/image.465732423.4753/flat,1000x1000,075,f.jpg');
+  }
+}
