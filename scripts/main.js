7bbf546921bf5fe44502945532f3011fe650510f
MasMiftah <74440247+MasMiftah@users.noreply.github.com>
2020-11-16 20:34:17 -0600

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -8,3 +8,27 @@ myImage.onclick = function() {
     myImage.setAttribute('src', 'images/questionmark.png');
   }
 }
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUsersName() {
+  let myName = prompt('Please enter your user name.');
+  if(!myName) {
+    setUserName();
+  } else { 
+    localStorage.setItem('name', myName);
+    myheading.textContent = 'Hey, ' + myName + ', Click the question mark to see the most popular destination our users have selected!';
+  }
+}
+
+if(!localStorage.getItem('name')){
+  setUsersName();
+} else {
+  let storedName = localStorage.getItem('name');
+  myHeading.innerHTML = 'Hey, ' + myName + ', Click the question mark to see the most popular destination our users have selected!';
+}
+
+myButton.onclick = function() {
+  setUsersName();
+}
