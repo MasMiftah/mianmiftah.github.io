6c46a9fe959e8b969afc2e72f9ea4a21a4a790b2
MasMiftah <74440247+MasMiftah@users.noreply.github.com>
2020-11-16 19:51:46 -0600

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -2,9 +2,9 @@ let myImage = document.querySelector('img');
 
 myImage.onclick = function() {
   let mySrc = myImage.getAttribute('src');
-  if (mySrc == 'https://ih1.redbubble.net/image.465732423.4753/flat,1000x1000,075,f.jpg'){
+  if (mySrc == 'https://www.nicepng.com/png/detail/167-1677510_riddler-question-mark-question-mark.png'){
     myImage.setAttribute('src', 'https://www.pngkey.com/png/full/18-182565_eiffel-tower-500-x-500.png');
   }else{
-    myImage.setAttribute('src', 'https://ih1.redbubble.net/image.465732423.4753/flat,1000x1000,075,f.jpg');
+    myImage.setAttribute('src', 'https://www.nicepng.com/png/detail/167-1677510_riddler-question-mark-question-mark.png');
   }
 }
