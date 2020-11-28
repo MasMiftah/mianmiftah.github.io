97baf4e3dc0e365c5078d589a0b1e8f3f47930dc
MasMiftah <74440247+MasMiftah@users.noreply.github.com>
2020-11-27 18:27:48 -0600

    Update main2.js

--- scripts/main2.js
+++ scripts/main2.js
@@ -27,28 +27,28 @@ let myImage = document.querySelector('img');
 // }
 
 myImage.onclick = function() {
-  var images = [{src : "images/greatwall.jpg", width : "500", height : "500"},
-                {src : "images/Eiffel.jpg", width : "500", height : "500"}];
-  var pre_buffer = [];
-  for (var i = 0; j = images.length; i < j; i++){
-    pre_buffer[i] = new Image();
-    pre_buffer[i].src = images[i].src;
-    pre_buffer[i].width = images[i].width;
-    pre_buffer[i].height = images[i].height;
-  }
+//   var images = [{src : "images/greatwall.jpg", width : "500", height : "500"},
+//                 {src : "images/Eiffel.jpg", width : "500", height : "500"}];
+//   var pre_buffer = [];
+//   for (var i = 0; j = images.length; i < j; i++){
+//     pre_buffer[i] = new Image();
+//     pre_buffer[i].src = images[i].src;
+//     pre_buffer[i].width = images[i].width;
+//     pre_buffer[i].height = images[i].height;
+//   }
   
-  function get_rand_int(min, max){
-    im_num = Math.floor(Math.random() * (max - min + 1)) + min;
-    return pre_buffer[im_num];
-  }
+//   function get_rand_int(min, max){
+//     im_num = Math.floor(Math.random() * (max - min + 1)) + min;
+//     return pre_buffer[im_num];
+//   }
   
-  var new_image = get_rand_int(0, pre_buffer.length - 1);
-  var image = document.getElementsByTagName('img');
-  var l = image.length;
-  for (var p = 0; p < l; p++){
-    image[0].parentNode.removeChild(images[0]);
-  }
-  document.body.appendChild(new_image);
+//   var new_image = get_rand_int(0, pre_buffer.length - 1);
+//   var image = document.getElementsByTagName('img');
+//   var l = image.length;
+//   for (var p = 0; p < l; p++){
+//     image[0].parentNode.removeChild(images[0]);
+//   }
+//   document.body.appendChild(new_image);
   let mySrc = myImage.getAttribute('src');
   if (mySrc == 'images/questionmark.png'){
     myImage.setAttribute('src', 'images/greatwall.jpg');
