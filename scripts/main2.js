188e2ba95f9ce32c813e38acfa8089ec583a6325
MasMiftah <74440247+MasMiftah@users.noreply.github.com>
2020-11-27 18:26:34 -0600

    Update main2.js

--- scripts/main2.js
+++ scripts/main2.js
@@ -1,6 +1,32 @@
 let myImage = document.querySelector('img');
 
-function display_random_image(){
+// function display_random_image(){
+//   var images = [{src : "images/greatwall.jpg", width : "500", height : "500"},
+//                 {src : "images/Eiffel.jpg", width : "500", height : "500"}];
+//   var pre_buffer = [];
+//   for (var i = 0; j = images.length; i < j; i++){
+//     pre_buffer[i] = new Image();
+//     pre_buffer[i].src = images[i].src;
+//     pre_buffer[i].width = images[i].width;
+//     pre_buffer[i].height = images[i].height;
+//   }
+  
+//   function get_rand_int(min, max){
+//     im_num = Math.floor(Math.random() * (max - min + 1)) + min;
+//     return pre_buffer[im_num];
+//   }
+  
+//   var new_image = get_rand_int(0, pre_buffer.length - 1);
+//   var image = document.getElementsByTagName('img');
+//   var l = image.length;
+//   for (var p = 0; p < l; p++){
+//     image[0].parentNode.removeChild(images[0]);
+//   }
+//   document.body.appendChild(new_image);
+  
+// }
+
+myImage.onclick = function() {
   var images = [{src : "images/greatwall.jpg", width : "500", height : "500"},
                 {src : "images/Eiffel.jpg", width : "500", height : "500"}];
   var pre_buffer = [];
@@ -23,17 +49,12 @@ function display_random_image(){
     image[0].parentNode.removeChild(images[0]);
   }
   document.body.appendChild(new_image);
-  
-}
-
-myImage.onclick = function() {
-  display_random_image();
-//   let mySrc = myImage.getAttribute('src');
-//   if (mySrc == 'images/questionmark.png'){
-//     myImage.setAttribute('src', 'images/greatwall.jpg');
-//   }else{
-//     myImage.setAttribute('src', 'images/questionmark.png');
-//   }
+  let mySrc = myImage.getAttribute('src');
+  if (mySrc == 'images/questionmark.png'){
+    myImage.setAttribute('src', 'images/greatwall.jpg');
+  }else{
+    myImage.setAttribute('src', 'images/questionmark.png');
+  }
 }
 
 let myButton = document.querySelector('button2');
